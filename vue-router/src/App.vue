8a2311b9e90b6e29535cf5d74a7be65b212e3d4b
Kyle Piacenta <124377155+kpiacenta93@users.noreply.github.com>
2024-01-01 14:10:16 -0500

    got router working, styling see Applications page

--- vue-router/src/App.vue
+++ vue-router/src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    <h1>Header</h1>
-    <router-link to="/">Home</router-link>
-    <router-link to="/SeeApplications">See Applications</router-link>
-    <router-link to="/AddApplications">Add Applications</router-link>
+    <header class="nav-bar">
+      <div class="nav-links">
+        <router-link to="/">Home|</router-link>
+        <router-link to="/SeeApplications"> See Applications|</router-link>
+        <router-link to="/AddApplications"> Add Applications</router-link>
+      </div>
+      <div class="contact-button">
+        <button class="contact">Contact Us!</button>
+      </div>
+    </header>
     <router-view />
   </div>
 </template>
+
 
 
 <script>
@@ -15,7 +22,68 @@ export default {
 }
 </script>
 
-<script>
+<style scoped>
+body {
+  margin: 0;
+  background-color: whitesmoke;
+}
+
+.nav-bar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+  padding: 10px 20px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1000;
+  
+}
+
+.nav-links {
+  display: flex;
+  list-style: none;
+  padding-right: 10px;
+  justify-content: space-between;
+  font-family: monospace;
+  /* margin: 0; */
+  font-size: large;
+  color: black;
+  border:5px;
+  
+}
+
+.nav-links:hover {
+  text-decoration: 1px solid black ;
+}
+
+.nav-links router-link {
+  margin-right: 20px; /* Add margin between router links */
+  text-decoration: none;
+  color: black;
+  font-weight: bold;
+  padding: 20px;
+  
+}
 
 
-</script>
+.contact-button {
+  display: flex;
+  align-items: center;
+  font-family: monospace;
+}
+
+.contact {
+  height: 40px;
+  border-radius: 5px;
+  text-align: center;
+  padding: 5px 10px;
+  background-color: rgb(92, 212, 246);
+  color: whitesmoke;
+  font-weight: bold;
+}
+</style>
+
--- vue-router/src/views/SeeApplications.vue
+++ vue-router/src/views/SeeApplications.vue
@@ -1,5 +1,9 @@
 <template>
-<h6>See Apps</h6>
+
+<div class="container">
+    <div class="seeCalendar">See Calendar</div>
+    <div class="searchApps">Search Apps</div>
+</div>
 
 </template>
 
@@ -12,5 +16,49 @@ export default {
 </script>
 
 <style>
+/* .title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-bottom: 750px;
+    font-family: monospace;
+    font-size: 2rem;
+    text-decoration: underline;
+} */
 
+.container {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    
+}
+
+.seeCalendar, .searchApps {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    border: 1px solid white;
+    width: 400px;
+    height: 400px;
+    transition: all;
+    font-family: monospace;
+    font-size: x-large;
+    cursor: pointer;
+    
+}
+
+.searchApps:hover {
+    transform: scaleY(1.3rem);
+    background-color: rgb(92, 212, 246);
+    text-decoration: underline;
+    border: 1px solid black;
+}
+
+.seeCalendar:hover {
+    transform: scaleY(1.3rem);
+    background-color: rgb(92, 212, 246);
+    text-decoration: underline;
+    border: 1px solid black;
+}
 </style>
