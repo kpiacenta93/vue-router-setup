e391dbd2efc30bed158560b9a1f1c27ea76f67f4
Kyle Piacenta <124377155+kpiacenta93@users.noreply.github.com>
2024-01-01 16:22:34 -0500

    adding styles to searchApplications

--- vue-router/src/views/SearchApplicationsViews.vue
+++ vue-router/src/views/SearchApplicationsViews.vue
@@ -1,7 +1,15 @@
 <template>
 <div class="container">
+    
  <div class="jobsList">
+    <div class="input">
+    <input type="text" placeholder="Search Job By Text...">
+    <div class="button">
+    <button>Submit</button>
+    </div>
+    </div>
     <li v-for="(application, index) in jobApplications" :key="index" class="jobs">
+    
         <h2 class="title">{{ application.jobTitle }}</h2>
         <p>Company: {{ application.company }}</p>
         <p>Application Date: {{ application.applicationDate }}</p>
@@ -10,6 +18,7 @@
         <p>Contact Phone Number: {{ application.contactPhoneNumber }}</p>
         <p>Application Status: {{ application.applicationStatus }}</p>
         <p>Notes: {{ application.notes }}</p>
+ 
         <button class="update">Update</button>
       </li>
  </div>
@@ -57,7 +66,7 @@ export default {
     contactEmail: "emily.johnson@example.com",
     contactPhoneNumber: "555-123-4567",
     applicationStatus: "Viewed",
-    notes: "Received a confirmation email after applying."
+    notes: "Received a confirmation email after applying. Apparently he pooped in his pants during the interview and that really put the interviewer off but he thinks he can redeem himself"
   },
   {
     jobTitle: "UX Designer",
@@ -90,12 +99,17 @@ export default {
 
 .jobs {
     display:flex;
-    flex-direction: column;
+    flex-direction: row;
     justify-content: center;
-    align-items: center;
+    align-items:center;
     border: 1px solid white;
     padding-top: 5px;
     transition: background-color 0.3s;
+    height: 200px;
+    padding-left: 5px;
+    font-size: 0.8rem;
+    font-family: monospace;
+    width: 1200px;
     
 }
 
@@ -112,5 +126,25 @@ export default {
 
 .update {
   margin-bottom: 8px;
+  margin-right: 5px;
+}
+
+.update:hover{
+    border: #535bf2;
+}
+
+.input{
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    margin-bottom: 10px;
+    height: 50px;
+
+}
+
+.individual-jobs{
+    display: flex;
+    justify-content: center;
+    border: 1px solid white;
 }
 </style>
