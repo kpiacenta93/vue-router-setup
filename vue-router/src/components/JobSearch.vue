67ea0bf1400225be37018c013a2d1b8ca2d5fbe5
Kyle Piacenta <124377155+kpiacenta93@users.noreply.github.com>
2024-03-07 10:19:36 -0500

    adding new job search 3rd party API to search jobs component

--- vue-router/src/components/JobSearch.vue
+++ vue-router/src/components/JobSearch.vue
@@ -1,85 +1,88 @@
 <template>
-    <div class="input-container">
-      <div class="input-wrapper">
-        <input type="text" placeholder="Search Jobs..." class="SearchJobs" v-model="jobTitle">
-        <input type="text" placeholder="Location..." class="SearchJobs" v-model="location">
-        <button class="button-search" @click="fetchJobs">Enter</button>
-      </div>
-      <div v-if="isLoading" class="loading">Loading...</div>
-      <div class="job-display" v-if="!isLoading && showJobs">
-        <div class="job-list">
-          <div v-for="(job, index) in jobListings" :key="index" class="jobs">
-            <h2>Job Title: {{ job.title }}</h2>
-            <br>
-            <h3>Company Name: {{ job.companyName }}</h3>
-            <br>
-            <p><b>Description:</b> {{ job.description }}</p>
-            <h2>Compensation: {{ job.pay }}</h2>
-            <br>
-            <a :href="job.companyUrl" target="_blank">Apply</a>
-          </div>
+  <div class="input-container">
+    <div class="input-wrapper">
+      <input type="text" placeholder="Search Jobs By Title and Location..." class="SearchJobs" v-model="query"> <!-- Ensure you have a model for 'query' -->
+      <!-- <input type="text" placeholder="Location..." class="SearchJobs" v-model="location"> -->
+      <button class="button-search" @click="fetchJobs">Enter</button>
+    </div>
+    <div v-if="isLoading" class="loading">Loading...</div>
+    <div class="job-display" v-if="!isLoading && showJobs">
+      <div class="job-list">
+        <div v-for="(job, index) in jobListings" :key="index" class="jobs">
+          <h2> Company: {{ job.employer_name }}</h2>
+          <img :src="job.employer_logo" :alt="job.employer_name + ' logo'" onerror="this.style.display='none'" class="employee-logo" />
+          <p><b>Job Title :</b> {{ job.job_title }}</p>
+          <h2>Description: {{ job.job_description }}</h2>
+          <a :href="job.employer_website" target="_blank">Apply</a>
         </div>
       </div>
     </div>
-  </template>
-  
+  </div>
+</template>
 
-  <script>
-  import axios from 'axios';
+<script>
+import axios from 'axios';
 
-  export default {
-    data() {
-      return {
-       
-        jobTitle: "",
-        location: "",
-        showJobs: false,
-        isLoading: false,
-        jobListings: []
-      };
-    },
-    methods: {
-      async fetchJobs() {
-        this.isLoading = true;
-        this.showJobs = false;
-        const options = {
-          method: 'POST',
-          url: 'https://linkedin-jobs-scraper-api.p.rapidapi.com/jobs',
-          headers: {
-            'content-type': 'application/json',
-            'X-RapidAPI-Key': import.meta.env.VITE_VUE_APP_RAPID_API_KEY,
-            'X-RapidAPI-Host': 'linkedin-jobs-scraper-api.p.rapidapi.com'
-          },
-          data: {
-            title: this.jobTitle,
-            location: this.location,
-            rows: 25
-          }
-
-        };
-  
-        try {
-          const response = await axios.request(options);
-          this.jobListings = response.data; 
-          this.showJobs = true;
-        } catch (error) {
-          console.error(error);
-        } finally {
-          this.isLoading = false;
-        }
-
-        if(this.jobListings.length === 0){
-            alert("your query had no results... Please try again!")
-            this.jobTitle = "";
-            this.location = "";
-        }
+export default {
+data() {
+  return {
+    query: "",
+    location: "", 
+    page: "1", 
+    showJobs: false,
+    isLoading: false,
+    jobListings: []
+  };
+},
+methods: {
+  async fetchJobs() {
+    this.isLoading = true;
+    this.showJobs = false;
+    const options = {
+      method: 'GET',
+      url: 'https://jsearch.p.rapidapi.com/search',
+      params: {
+        query: this.query,
+        page: this.page,
       },
-    } 
-  }
-  </script>
+      headers: {
+        'X-RapidAPI-Key': import.meta.env.VITE_VUE_SEARCH_JOBS_API_KEY,
+        'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+      }
+    };
+
+    try {
+      const response = await axios.request(options);
+      this.jobListings = response.data.data; 
+      console.log("This is the job data: ",this.jobListings)
+      this.showJobs = true;
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.isLoading = false;
+    }
+
+    if (this.jobListings.length === 0) {
+      alert("Your query had no results... Please try again!");
+      this.query = "";
+      
+    }
+  },
+}
+}
+</script>
+
   
 
 <style>
+
+.employee-logo {
+  /* display: flex; */
+  /* justify-content: center; */
+  width: 500px;
+  align-items: center;
+  height: 200px
+}
 .jobs {
     border: 1px solid white;
     margin: 10px;
@@ -88,11 +91,12 @@
 
 .input-container {
     display: flex;
-    justify-content: center;
+    justify-content:baseline;
     align-items: center;
     flex-direction: column;
-    margin: 0px;
+    margin: 70px;
     height: 100vh;
+    
 }
 
 .input-wrapper {
