2c0b4914fd9287aff29df1c18294b3a2af5ee6a7
Kyle Piacenta <124377155+kpiacenta93@users.noreply.github.com>
2024-03-20 09:59:10 -0400

    setting up emailJS API

--- vue-router/src/components/Random.vue
+++ vue-router/src/components/Random.vue
@@ -1,25 +1,24 @@
 <template>
     <div class="form-container">
-      <form @submit.prevent="submitForm" class="form">
+      <form @submit.prevent="sendEmail" class="form">
         <h5 class="title">Contact Us!</h5>
-        <label for="name">Name:</label>
+        <label for="name">Name: {{ name }}</label>
         <input type="text" id="name" v-model="name" required class="form-input">
   
-        <label for="email">Email:</label>
+        <label for="email">Email: {{ email }}</label>
         <input type="email" id="email" v-model="email" required class="form-input">
   
-        <label for="subject">Subject:</label>
-        <input type="text" id="subject" v-model="subject" required class="form-input">
-  
-        <label for="message">Message:</label>
+        <label for="message">Message: {{ message }}</label>
         <textarea id="message" v-model="message" rows="4" required class="form-textarea"></textarea>
   
-        <button type="submit" v-on:click="consoleLogFormData()">Submit</button>
+        <button type="submit" value="Send">Submit</button>
       </form>
     </div>
   </template>
 
 <script>
+
+import emailjs from 'emailjs-com';
 
 
 export default {
@@ -27,18 +26,31 @@ export default {
 
 data(){
     return{
-        name: '',
-        email: '',
-        subject: '',
+        from_name: '',
+        from_email: '',
         message: '',
     }
 },
 
 methods: {
-        consoleLogFormData(){
-            console.log("this is name: ", this.name)
+    sendEmail(e) { 
+        try {
+            emailjs.sendForm(import.meta.env.VITE_VUE_EMAIL_JS_SERVICE_ID, import.meta.env.VITE_VUE_EMAIL_JS_TEMPLATE_ID, e.target, import.meta.env.VITE_VUE_EMAIL_JS_USER_ID)
+            .then((result) => {
+                console.log('Email successfully sent!', result.text);
+            }, (error) => {
+                console.log('Failed to send email.', error.text);
+            });
+        } catch (error) {
+            console.log({error});
         }
-},
+        // Reset form fields
+        this.from_name = '';
+        this.from_email = '';
+        this.message = '';
+    },
+}
+
 
 
 
